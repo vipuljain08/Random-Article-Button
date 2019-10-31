3f911bdc54c829e5178f93206e7b9398de856334
bilalsevis <bilalresmi@gmail.com>
2019-10-31 15:23:35 +0300

    "Update"

--- src/App.js
+++ src/App.js
@@ -1,26 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Database from './data/Articles';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [articles, setArticles]  = useState([])
+
+    useEffect( () => {
+        getArticles();
+    }, [])
+
+    
+    const getArticles = async (e) => {
+
+        /* You can also use a fake rest api for fetching data with using typicode's json-server */
+        
+        // const response = await fetch(`http://localhost:3001/articles`) 
+        // const data = response.json()
+
+        const data = await Database
+
+        // Create array of object data
+        const values = Object.values(data)
+
+        // Generate random index based on number of data
+        const randomArticle = values[Math.floor(Math.random() * values.length)] 
+        console.log(randomArticle)
+        setArticles(randomArticle)
+    }
+    return (
+        <div className="App">
+            <div className="container mt-5 mb-4">
+                <div className="row">
+                    <div className="col-lg-12 mb-4 home-header">
+                        <center>
+                            <h1 className="head-title mb-5">
+                            Discover a new article at 
+                            <br/> the click of a button <span role="img" aria-label="🙌 "> 🙌 </span> 
+                            </h1>
+                            <button className="btn btn-outline-secondary" onClick={getArticles}>Give me another random article</button>
+                        </center>
+                    </div>
+                    <div className="col-lg-12 mt-4 article-title">
+                        <h2 className="mb-5">Here is your random article</h2>
+                        <div className="container article-title-link">
+                            <div className="row">
+                                <div className="col-lg-12">
+                                <a href={articles.url} target="_blank" rel="noopener noreferrer">
+                                    
+                                    <h2 className="mt-4 mb-4"> {articles.name} </h2>
+                                </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/data/Articles.js
@@ -0,0 +1,85 @@
+ const articles = [
+    {
+       "id":1,
+       "name": "22 Performance Optimization Techniques for React",
+       "url":"https://medium.com/better-programming/https-medium-com-mayank-gupta-6-88-21-performance-optimizations-techniques-for-react-d15fa52c2349",
+       "email":null,
+       "published":"0",
+       "created_at":"2019-10-17 12:40:23",
+       "updated_at":"2019-10-17 12:40:23"
+    },
+    {
+       "id":2,
+       "name": "React JS — Understanding Functional & Class Components",
+       "url":"https://codeburst.io/react-js-understanding-functional-class-components-e65d723e909",
+       "email":null,
+       "published":"0",
+       "created_at":"2019-10-17 13:07:48",
+       "updated_at":"2019-10-17 13:07:48"
+    },
+    {
+       "id":3,
+       "name": "Demystifying React Hooks",
+       "url":"https://medium.com/better-programming/demystifying-react-hooks-a0b56a6254c2",
+       "email":null,
+       "published":"0",
+       "created_at":"2019-10-17 13:07:48",
+       "updated_at":"2019-10-17 13:07:48"
+    },
+    {
+       "id":4,
+       "name": "14 Beneficial Tips to Write Cleaner Code in React Apps",
+       "url":"https://medium.com/better-programming/14-beneficial-tips-to-write-cleaner-code-in-react-apps-a167798fa1ba",
+       "email":null,
+       "published":"0",
+       "created_at":"2019-10-17 13:07:48",
+       "updated_at":"2019-10-17 13:07:48"
+    },
+    {
+       "id":5,
+       "name": "How to build reusable layouts in React JS",
+       "url":"https://medium.com/javascript-in-plain-english/how-to-build-reusable-layouts-in-react-js-daf8adcbca79",
+       "email":null,
+       "published":"0",
+       "created_at":"2019-10-17 13:07:48",
+       "updated_at":"2019-10-17 13:07:48"
+    },
+    {
+       "id":6,
+       "name": "22 Miraculous Tools for React Developers in 2019",
+       "url":"https://medium.com/better-programming/22-miraculous-tools-for-react-developers-in-2019-7d72054e2306",
+       "email":null,
+       "published":"0",
+       "created_at":"2019-10-17 13:07:48",
+       "updated_at":"2019-10-17 13:07:48"
+    },
+    {
+       "id":7,
+       "name": "Build a Chatbot in 10 minutes with React",
+       "url":"https://medium.com/javascript-in-plain-english/may-i-help-you-build-a-chatbot-in-10-minutes-with-react-df19e940bbc8",
+       "email":null,
+       "published":"0",
+       "created_at":"2019-10-17 13:07:48",
+       "updated_at":"2019-10-17 13:07:48"
+    },
+    {
+       "id":8,
+       "name": "React Virtual DOM Postmortem",
+       "url":"https://medium.com/@js_tut/react-animated-tutorial-7a46fa3c2b96",
+       "email":null,
+       "published":"0",
+       "created_at":"2019-10-17 13:07:48",
+       "updated_at":"2019-10-17 13:07:48"
+    },
+    {
+       "id":9,
+       "name": "Building Basic React Authentication",
+       "url":"https://medium.com/better-programming/building-basic-react-authentication-e20a574d5e71",
+       "email":null,
+       "published":"0",
+       "created_at":"2019-10-17 13:07:48",
+       "updated_at":"2019-10-17 13:07:48"
+    }
+ ]
+
+ export default articles
