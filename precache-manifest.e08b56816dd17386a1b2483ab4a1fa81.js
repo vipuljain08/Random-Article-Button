4af76a55b0676a20c89555c8cd9ed14004025f1b
bilalsevis <bilalresmi@gmail.com>
2019-10-31 15:39:41 +0300

    Updates

---
+++ precache-manifest.e08b56816dd17386a1b2483ab4a1fa81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3018cf389e7aef23c04e5eaa361e6aa",
+    "url": "/Random-Article-Button/index.html"
+  },
+  {
+    "revision": "6fa4f5283c55d6673f58",
+    "url": "/Random-Article-Button/static/css/main.e6b83666.chunk.css"
+  },
+  {
+    "revision": "f3fa51fde27ba3615fec",
+    "url": "/Random-Article-Button/static/js/2.ca698f8b.chunk.js"
+  },
+  {
+    "revision": "6fa4f5283c55d6673f58",
+    "url": "/Random-Article-Button/static/js/main.7be94ea6.chunk.js"
+  },
+  {
+    "revision": "aeb82b463482ce9e1728",
+    "url": "/Random-Article-Button/static/js/runtime-main.c3c61b21.js"
+  }
+]);
